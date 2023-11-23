0ec4938865a00f98a11333966beedc07cc256539
adityapanchal10 <panchaladitya007@gmail.com>
2023-11-23 16:42:29 +0100

    faster implementation

--- UAR.py
+++ UAR.py
@@ -67,40 +67,47 @@ def calculate_genetic_distance(row):
     return genetic_distance
 
 # Calculate maxUAR1(B) and maxUAR2(B) for each row
-def calculate_max_uar_b(row):
+def calculate_max_uar(row):
     animal_i = row['animal_id_1']
     population_i = row['population_animal_id_1_belongs_to']
     
     # Filter rows where animal_id_1 is different from current animal_i and from a different population
     foreign_breeds = df[(df['animal_id_1'] != animal_i) & (df['population_animal_id_1_belongs_to'] != population_i)]
-    # print(foreign_breeds)
     
+    # For Max_UAR1(B) and Max_UAR2(B) 
     # Get the UAR scores between animal_i i and animals from different populations
     uar_values = foreign_breeds[foreign_breeds['animal_id_2'] == animal_i]['UAR score']
-    
     # Sort and get the highest (maxUAR1(B)) and second highest UAR score (maxUAR2(B))
     sorted_uar_values = uar_values.sort_values(ascending=False)
     max_uar1_b = sorted_uar_values.iloc[0] if len(sorted_uar_values) > 0 else 0.0
     max_uar2_b = sorted_uar_values.iloc[1] if len(sorted_uar_values) > 1 else 0.0
-    
-    return max_uar1_b, max_uar2_b
 
-# Calculate maxUAR(P)
-def calculate_max_uar_p(row):
-    animal_i = row['animal_id_1']
-    population_i = row['population_animal_id_1_belongs_to']
-    
-    # Filter rows where animal_id_1 is different from current animal_i and from a different population
-    foreign_breeds = df[(df['animal_id_1'] != animal_i) & (df['population_animal_id_1_belongs_to'] != population_i)]
+    # For Max_UAR(p) 
     foreign_breeds = foreign_breeds[(foreign_breeds['animal_id_2'] == animal_i)]
-    
     # Group by the breed of animal_id_1 and calculate the mean UAR for each breed
     breed_avg_uar = foreign_breeds.groupby('population_animal_id_1_belongs_to')['UAR score'].mean()
-    
     # Get the maximum average UAR across breeds
     max_uar_p = breed_avg_uar.max()
+    max_uar_p = max_uar_p if not pd.isnull(max_uar_p) else 0.0
+
+    return max_uar1_b, max_uar2_b, max_uar_p
+
+# Calculate maxUAR(P)
+# def calculate_max_uar_p(row):
+#     animal_i = row['animal_id_1']
+#     population_i = row['population_animal_id_1_belongs_to']
     
-    return max_uar_p if not pd.isnull(max_uar_p) else 0.0  # If no interactions with foreign breeds, return 0
+#     # Filter rows where animal_id_1 is different from current animal_i and from a different population
+#     foreign_breeds = df[(df['animal_id_1'] != animal_i) & (df['population_animal_id_1_belongs_to'] != population_i)]
+#     foreign_breeds = foreign_breeds[(foreign_breeds['animal_id_2'] == animal_i)]
+    
+#     # Group by the breed of animal_id_1 and calculate the mean UAR for each breed
+#     breed_avg_uar = foreign_breeds.groupby('population_animal_id_1_belongs_to')['UAR score'].mean()
+    
+#     # Get the maximum average UAR across breeds
+#     max_uar_p = breed_avg_uar.max()
+    
+#     return max_uar_p if not pd.isnull(max_uar_p) else 0.0  # If no interactions with foreign breeds, return 0
 
 # Get unique animal_id values
 unique_animals = pd.unique(df[['animal_id_1', 'animal_id_2']].values.ravel('K'))
@@ -110,8 +117,8 @@ result_data = []
 for animal_i in unique_animals:
     temp = df[df['animal_id_1'] == animal_i].head(n=1)
     gd = temp.apply(calculate_genetic_distance, axis=1).values[0]
-    max_uar1_b, max_uar2_b = temp.apply(calculate_max_uar_b, axis=1).values[0]
-    max_uar_p = temp.apply(calculate_max_uar_p, axis=1).values[0]
+    max_uar1_b, max_uar2_b, max_uar_p = temp.apply(calculate_max_uar, axis=1).values[0]
+    # max_uar_p = temp.apply(calculate_max_uar_p, axis=1).values[0]
     animal_data = {
         'Animal_id': animal_i,
         'Genetic Distance': gd,  
